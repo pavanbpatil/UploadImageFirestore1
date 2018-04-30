334d5a12a75bc67b99bc9d2976cc8af959c70367
mindwings <pavanpatil.co1@gmail.com>
2018-04-30 15:32:32 +0530

    fifth commit

--- app/src/main/java/com/mindwings/uploadimagefirestore/MainActivity.java
+++ app/src/main/java/com/mindwings/uploadimagefirestore/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     ProgressBar progressbar;
     EditText imagename;
         String abc;
+        int p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
